e7c56a312e2077dbaab5fcc2f0d0e702b46f0aeb
Mahidharan <mahidharan5612@gmail.com>
2025-08-02 18:01:49 +0530

    Final add

---
+++ script.js
@@ -0,0 +1,6 @@
+function openLogin() {
+  document.getElementById("loginOverlay").style.display = "flex";
+}
+function closeLogin() {
+  document.getElementById("loginOverlay").style.display = "none";
+}
