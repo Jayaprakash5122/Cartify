c215372ca7d7ea8f599283b6440929e30950ac17
Mahidharan <mahidharan5612@gmail.com>
2025-08-05 22:43:19 +0530

    Final

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function openLogin() {
 function closeLogin() {
   document.getElementById("loginOverlay").style.display = "none";
 }
+// Toggle the mobile menu
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('active');
+}
